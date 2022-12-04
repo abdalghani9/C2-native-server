47cf91847e0698888821a95d323e8d3e294c7566
abdalghani9 <abdalghani999@gmail.com>
2022-12-04 16:37:43 +0400

    add get to send page

--- Server/index.js
+++ Server/index.js
@@ -16,6 +16,10 @@ const db = mysql.createConnection({
 
 let insert = "INSERT INTO messages (name,email,message)  VALUES (?,?,?)";
 
+app.get("/send",(req,res)=>{
+  res.write("Welcome to send page");
+})
+
 app.post("/send", (req, res) => {
   const uname = req.body.uname;
   const email = req.body.email;
