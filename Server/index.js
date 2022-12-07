090eaf774188352c19474d1b3a7fa93f19962f1b
abdalghani9 <abdalghani999@gmail.com>
2022-12-07 20:14:19 +0400

    add cors objects param

--- Server/index.js
+++ Server/index.js
@@ -5,25 +5,25 @@ import mysql from "mysql";
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-// app.use(
-//   cors({
-//     origin: `${process.env.CLIENT_URL}`,
-//     methods: "GET,POST,PUT,DELETE",
-//     credentials: true,
-//   })
-// );
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: `${process.env.CLIENT_URL}`,
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
+
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+
+// app.use(cors());
 
 const db = mysql.createConnection({
   host: "mogo", //localhost
