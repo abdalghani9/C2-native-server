e51626f6f31f3d35e57608cd3fe6a4ff57420f5d
abdalghani9 <abdalghani999@gmail.com>
2022-12-07 21:10:19 +0400

    cors udpate

--- Server/index.js
+++ Server/index.js
@@ -1,16 +1,14 @@
 import express from "express";
-import cors from "cors";
 import mysql from "mysql";
-
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+app.use(cors(corsOptions)) // Use this after the variable declaration
 const app = express();
-// app.use(cors());
-app.use(
-  cors({
-    origin: `${process.env.CLIENT_URL}`,
-    methods: "GET,POST,PUT,DELETE",
-    credentials: true,
-  })
-);
+
 app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
