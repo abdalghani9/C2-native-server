2fed6738a2d6660ad9213b540b430ea1a1ccb220
abdalghani9 <abdalghani999@gmail.com>
2022-12-07 21:06:27 +0400

    use cors udpate

--- Server/index.js
+++ Server/index.js
@@ -3,16 +3,15 @@ import cors from "cors";
 import mysql from "mysql";
 
 const app = express();
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: `${process.env.CLIENT_URL}`,
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
 app.use(express.json());
-
-// app.use(
-//   cors({
-//     origin: `${process.env.CLIENT_URL}`,
-//     methods: "GET,POST,PUT,DELETE",
-//     credentials: true,
-//   })
-// );
 
 const PORT = process.env.PORT || 3001;
 
